ea0bbde5eab91b1f4e6215ee13d4fc3f10c50f32
sabrinamillane <brinaftnahiz@gmail.com>
2024-10-27 07:31:57 -0400

    feat(pdi): Capa da visibilidade - construção base

--- trabalho1.py
+++ trabalho1.py
@@ -2,48 +2,39 @@ import cv2
 import numpy as np
 import time
 
-# Leitura da imagem da câmera
-video = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
+time.sleep(3)  
 
-# Armazenamento de um quadro antes do início do loop
-_, background = video.read()
-time.sleep(2)
-_, background = video.read()
+# fundo para o primeiro frame
+ret, fundo = cap.read()
+fundo = np.flip(fundo, axis=1) 
 
-# Janela de controle de cores hsv
-def mouse(pos):
-    pass
+# Atualmente tá com apenas a cor azul aqu
+lower_hsv = np.array([100, 150, 0]) 
+upper_hsv = np.array([130, 255, 255]) 
 
-janela = "Controle de cores HSV"
-cv2.namedWindow(janela)
-labels = {"H":["H_min", "H_max"],
-          "S":["S_min", "S_max"],
-          "V":["V_min", "V_max"]}
-cv2.createTrackbar(labels["H"][0], janela, 0, 180, mouse)
-cv2.createTrackbar(labels["S"][0], janela, 0, 255, mouse)
-cv2.createTrackbar(labels["V"][0], janela, 0, 255, mouse)
-cv2.createTrackbar(labels["H"][1], janela, 0, 180, mouse)
-cv2.createTrackbar(labels["S"][1], janela, 0, 255, mouse)
-cv2.createTrackbar(labels["V"][1], janela, 0, 255, mouse)
-
-
-# Utilizando detecção de cores
-while True:
-    ret, frame = video.read()
+while cap.isOpened():
+    ret, frame = cap.read() 
+    if not ret:
+        break
+    frame = np.flip(frame, axis=1) 
 
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    h_min = cv2.getTrackbarPos(labels["H"][0], janela)
-    s_min = cv2.getTrackbarPos(labels["S"][0], janela)
-    v_min = cv2.getTrackbarPos(labels["V"][0], janela)
-    h_max = cv2.getTrackbarPos(labels["H"][1], janela)
-    s_max = cv2.getTrackbarPos(labels["S"][1], janela)
-    v_max = cv2.getTrackbarPos(labels["V"][1], janela)
-    mask = cv2.inRange(hsv, np.array([h_min, s_min, v_min]), np.array([h_max, s_max, v_max]))
-    
-    cv2.imshow("Video", mask)
-    k = cv2.waitKey(5) & 0xFF
-    if k == 27:
+    mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
+
+    kernel = np.ones((3, 3), np.uint8)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=2)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, kernel, iterations=1)
+
+    mask_inv = cv2.bitwise_not(mask)
+    fundo_mascarado = cv2.bitwise_and(fundo, fundo, mask=mask)
+    objeto_visivel = cv2.bitwise_and(frame, frame, mask=mask_inv)
+    resultado = cv2.addWeighted(fundo_mascarado, 1, objeto_visivel, 1, 0)
+
+    cv2.imshow("Capa de Invisibilidade: David e Sabrina", resultado)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-video.release()
+cap.release()
 cv2.destroyAllWindows()
