cca67de95f3477149c87ee11d909e77f18d06a6d
DavidAugusto <dados.eng21@uea.edu.br>
2024-10-30 23:07:35 -0400

    final

--- trabalho1.py
+++ trabalho1.py
@@ -9,7 +9,7 @@ time.sleep(3)
 ret, fundo = cap.read()
 fundo = np.flip(fundo, axis=1) 
 
-# Atualmente tá com apenas a cor azul aqu
+# Intervalo do padrão HSV (cor azul)
 lower_hsv = np.array([100, 150, 0]) 
 upper_hsv = np.array([130, 255, 255]) 
 
@@ -19,13 +19,16 @@ while cap.isOpened():
         break
     frame = np.flip(frame, axis=1) 
 
+    #detecção da cor
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_hsv, upper_hsv)
+
 
     kernel = np.ones((3, 3), np.uint8)
     mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=2)
     mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, kernel, iterations=1)
 
+    #aplicação da máscara
     mask_inv = cv2.bitwise_not(mask)
     fundo_mascarado = cv2.bitwise_and(fundo, fundo, mask=mask)
     objeto_visivel = cv2.bitwise_and(frame, frame, mask=mask_inv)
